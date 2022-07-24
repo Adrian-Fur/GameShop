4a617cdac82e5155374b8ce79026e18681a399fe
Adrian Furmanek <adrian.furm@gmail.com>
2022-07-24 12:44:35 +0200

    Product Search Layout

--- GameShop/Client/Services/ProductService/ProductService.cs
+++ GameShop/Client/Services/ProductService/ProductService.cs
@@ -35,13 +35,13 @@
 
         public async Task<List<string>> GetProductSearchSuggestions(string searchText)
         {
-            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/products/searchsuggestions/{searchText}");
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
             return result.Data;
         }
 
         public async Task SearchProducts(string searchText)
         {
-            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/products/search/{searchText}");
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
 
             if (result != null && result.Data != null)
             {
